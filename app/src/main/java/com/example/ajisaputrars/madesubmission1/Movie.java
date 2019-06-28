bea7f5987aa12bffb3b85163925c7b7627dcd0b2
Ajisaputrars <Ajisaputrars@hotmail.com>
2019-06-29 01:42:10 +0700

    Adding Parcelable

--- app/src/main/java/com/example/ajisaputrars/madesubmission1/Movie.java
+++ app/src/main/java/com/example/ajisaputrars/madesubmission1/Movie.java
@@ -1,6 +1,9 @@
 package com.example.ajisaputrars.madesubmission1;
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
     private int id;
     private String title;
     private String overview;
@@ -55,4 +58,43 @@ public class Movie {
     public void setVote_average(double vote_average) {
         this.vote_average = vote_average;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.title);
+        dest.writeString(this.overview);
+        dest.writeString(this.date);
+        dest.writeDouble(this.vote_average);
+        dest.writeInt(this.image);
+    }
+
+    public Movie() {
+    }
+
+    protected Movie(Parcel in) {
+        this.id = in.readInt();
+        this.title = in.readString();
+        this.overview = in.readString();
+        this.date = in.readString();
+        this.vote_average = in.readDouble();
+        this.image = in.readInt();
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
