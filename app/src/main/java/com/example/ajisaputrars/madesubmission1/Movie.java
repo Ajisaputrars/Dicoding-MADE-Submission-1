52180cda45ce2bfe2becc0982a13d932e9416be4
Ajisaputrars <Ajisaputrars@hotmail.com>
2019-06-28 19:10:49 +0700

    Second Commit

---
+++ app/src/main/java/com/example/ajisaputrars/madesubmission1/Movie.java
@@ -0,0 +1,51 @@
+package com.example.ajisaputrars.madesubmission1;
+
+import java.util.Date;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String overview;
+    private String date;
+    private float vote_average;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(float vote_average) {
+        this.vote_average = vote_average;
+    }
+}
